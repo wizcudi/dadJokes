12bdbedff10b61e5391484b5fd119aee4c166481
Tony Saint-Amour <tonysaint-amour@Spooky-Mac.local>
2022-07-12 13:03:34 -0400

    updated design

--- script.js
+++ script.js
@@ -2,11 +2,27 @@
 const jokeEl = document.getElementById('joke');
 const jokeBtn = document.getElementById('jokeBtn');
 
+
 // this function allows jokeBtn to communicate with Joke APi to fetch new jokes
 jokeBtn.addEventListener('click', gerenrateJoke)
-
-
 gerenrateJoke()
+
+
+// using ASYNC/AWAIT
+// whenever you use await inside of a function you have to label that function 'async'
+// any promise you want to put in variable needs await infront of it
+async function gerenrateJoke(){
+    const config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
+    const res = await fetch('https://icanhazdadjoke.com', config)
+    const data = await res.json()
+    jokeEl.innerHTML = data.joke
+}
+
+
 
 // using .then()
 // to make code cleaner, we put headers in seperate variable
@@ -26,41 +42,6 @@ gerenrateJoke()
 //         jokeEl.innerHTML = data.joke
 //     })
 // }
-
-
-// using ASYNC/AWAIT
-// whenever you use await inside of a function you have to label that function 'async'
-// any promise you want to put in variable needs await infront of it
-async function gerenrateJoke(){
-    const config = {
-        headers: {
-            'Accept': 'application/json'
-        }
-    }
-    const res = await fetch('https://icanhazdadjoke.com', config)
-
-    const data = await res.json()
-
-    jokeEl.innerHTML = data.joke
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
